8ab6bf75f8aa86abe6ad148ec48eccbc540aa225
Andrey <multirez@interbellum.ru>
2014-05-31 20:03:17 +0300

    Level1 prefabs without functionality
    Level1 game design

--- PAC_MAN/Assets/Scripts/Engine/Engine.cs
+++ PAC_MAN/Assets/Scripts/Engine/Engine.cs
@@ -2,27 +2,29 @@
 using System.Collections;
 
 public class Engine : MonoBehaviour{
-	private class Ceil{
+	public static Engine instance;
+
+	public class Ceil{
 		public Tile tile=null;
 		public Trigger trigger=null;
 	}
-
-	private Ceil[,] ceils;
+	public Ceil[,] ceils;
 
 	public int levelWidth=10;
 	public int levelHeight=10;
 
+	#region Standart Functions
 	private void Awake(){
+		instance=this;
 		//---- initialize ceils
 		ceils=new Ceil[levelWidth,levelHeight];
 		ceils.Initialize();
 	}
-
-
 
 	private void OnDrawGizmos(){
 		Gizmos.color=new Color(0f, 0f, 0.5f, 0.3f);
 		Gizmos.DrawCube(new Vector3(levelWidth*0.5f-0.5f, 0f, levelHeight*0.5f-0.5f), 
 		                new Vector3(levelWidth, 0.1f, levelHeight));
 	}
+	#endregion
 }
